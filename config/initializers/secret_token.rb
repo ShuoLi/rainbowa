546639d6ac994af713b3e6eb8304e5152a4a11ff
Will Chou <chou3213@gmail.com>
2011-12-19 12:06:55 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youtubetimelinewiki::Application.config.secret_token = '5e91c28911fb2589304f2c04d6d4df82c9f19466aec2803b59b7480d08eed531f73e8c342ad473e2db4a1b604af1a680ca68587f7cba9dd31c8ab12326c88cf0'

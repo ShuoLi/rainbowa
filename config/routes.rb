6d123cde0c79aab031171c2fbf6e2fe933b3ef1a
Will Chou <chou3213@gmail.com>
2011-12-30 19:25:36 -0500

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Youtubetimelinewiki::Application.routes.draw do
 
+  get "home/index"
+
   match "/auth/:provider/callback" => "sessions#create"
   match "/signout" => "sessions#destroy", :as => :signout
   match '/auth/failure' => 'sessions#failure'
@@ -61,7 +63,7 @@ Youtubetimelinewiki::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'timelines#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 

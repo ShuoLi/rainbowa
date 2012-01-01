b087dab12c58e675e33b6817f2a351d942ac099e
Will Chou <chou3213@gmail.com>
2012-01-01 17:01:21 -0500

    facebook app setup done for production

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,10 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :facebook, '198081906950333', '67afe1e0075870faa1efb72fcfe6c123'
+  if Rails.env == 'development'
+    provider :facebook, '198081906950333', '67afe1e0075870faa1efb72fcfe6c123'
+  else
+    if Rails.env == 'production'
+      provider :facebook, '352806468068101', '50c352990839be14f0908fe1867b0730'      
+    end
+  end
 end
